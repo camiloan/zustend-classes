81fcb0bbd4dff5fab286625c7ca13c2feae5d6a5
Camilo Andres Bolaños Andrade <camiloa.andresc@hotmail.com>
2024-10-17 21:19:26 -0500

    update branch

--- zustand-dashboard/src/stores/storages/firebase.storage.ts
+++ zustand-dashboard/src/stores/storages/firebase.storage.ts
@@ -1,29 +1,29 @@
-import { StateStorage, createJSONStorage } from 'zustand/middleware';
+import type { StateStorage, createJSONStorage } from "zustand/middleware";
 
 const firebaseUrl =
-  'https://react-firebase-testing-20244-default-rtdb.firebaseio.com/zustand';
+	"https://react-firebase-testing-20244-default-rtdb.firebaseio.com/zustand";
 
 const storageApi: StateStorage = {
-  getItem: async function (name: string): Promise<string | null> {
-    try {
-      const data = await fetch(`${firebaseUrl}/${name}.json`).then((res) =>
-        res.json()
-      );
-      return JSON.stringify(data);
-    } catch (error) {
-      throw error;
-    }
-  },
-  setItem: async function (name: string, value: string): Promise<void> {
-    await fetch(`${firebaseUrl}/${name}.json`, {
-      method: 'PUT',
-      body: value,
-    }).then((res) => res.json());
-    return;
-  },
-  removeItem: function (name: string): void | Promise<void> {
-    sessionStorage.removeItem(name);
-  },
+	getItem: async function (name: string): Promise<string | null> {
+		try {
+			const data = await fetch(`${firebaseUrl}/${name}.json`).then((res) =>
+				res.json(),
+			);
+			return JSON.stringify(data);
+		} catch (error) {
+			throw error;
+		}
+	},
+	setItem: async function (name: string, value: string): Promise<void> {
+		await fetch(`${firebaseUrl}/${name}.json`, {
+			method: "PUT",
+			body: value,
+		}).then((res) => res.json());
+		return;
+	},
+	removeItem: function (name: string): void | Promise<void> {
+		sessionStorage.removeItem(name);
+	},
 };
 
 export const firebaseStorage = createJSONStorage(() => storageApi);
